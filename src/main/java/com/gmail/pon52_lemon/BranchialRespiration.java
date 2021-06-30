8ebe32a3f780d8239b07a0596d9b5f307d847104
pon52.lemon <85234696+pon52-lemon@users.noreply.github.com>
2021-06-30 23:01:27 +0900

    はじめてのこみっと

---
+++ src/main/java/com/gmail/pon52_lemon/BranchialRespiration.java
@@ -0,0 +1,13 @@
+package com.gmail.pon52_lemon;
+
+/**
+ * Hello world!
+ *
+ */
+public class BranchialRespiration 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
